5ccdd9a144495f9b50037eb6b901177cc8fcb636
GitHub Action <action@github.com>
2021-07-22 18:30:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/madhoma/androidblank/MyBundle.kt
+++ src/main/kotlin/com/github/madhoma/androidblank/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.madhoma.androidblank
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/madhoma/androidblank/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/madhoma/androidblank/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.madhoma.androidblank.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.madhoma.androidblank.MyBundle
 
 class MyApplicationService {
 
